badfab9a45b97b6b730fdb54ab12d1b657f6c1ff
Candy Obi <obiijeoma32@gmail.com>
2022-12-19 06:49:30 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -8,17 +8,20 @@ import "./App.css";
 export default function App() {
   const [text, setText] = useState("");
   const [result, setResult] = useState("");
+  const [calc, setCalc] = useState(false);
 
   function addToText(val) {
-    setText((text) => [...text, val + ""]);
+    setText((text) => [...text, val + ""].join(""));
+    // console.log(val + "");
   }
   function inputReset() {
     setText("");
     setResult("");
+    setCalc(false);
   }
   function calculateResult() {
     try {
-      const input = text.join("");
+      const input = text;
 
       setResult(math.evaluate(input));
     } catch (err) {
@@ -26,36 +29,52 @@ export default function App() {
     }
   }
   function calculateSquareRoot() {
-    const num = text.join("");
-    setResult(math.sqrt(num));
+    setResult(math.sqrt(math.evaluate(text)));
   }
 
   function cosine() {
     try {
-      const baby = text.join("");
+      const baby = text;
 
-      setResult(math.cos(baby));
+      setResult(math.cos(math.evaluate(baby)));
     } catch (err) {
       setResult("Error");
     }
   }
   function backSpace() {
     setText(text.slice(0, text.length - 1));
+    // setCalc(false);
   }
   function addMinus() {
-    try {
-      setResult(math.evaluate(text.join("")));
-    } catch (err) {
-      setResult("Error");
-    }
+    setCalc((val) => !val);
+
+    // const t = text.toString().replace(/\s/g, "");
+    // setText(t * -1, t * +1);
+    // const removeSpaces = (text) => text.toString().replace(/\s/g, "");
+    // const kj = text ? setText(text * -1) : setText(text * +1);
+    // console.log(kj);
+    // setResult({ ...result, text: result.text ? toLocaleString(removeSpaces(result.text)) * -1 : 0, result: result.text ? toLocaleString(removeSpaces(result.text)) * -1 : 0 });
+    // setResult(math.evaluate(kj.replace(/\s/g, "")));
+    // try {
+    //   setResult(math.evaluate(text.join("")));
+    // } catch (err) {
+    //   setResult("Error");
+    // }
+    // const removeSpaces = (text) => text.toString().replace(/\s/g, "");
+    // setCalc({
+    //   ...calc,
+    //   text: calc.text ? toLocaleString(removeSpaces(calc.text)) * -1 : 0,
+    //   result: calc.text ? toLocaleString(removeSpaces(calc.result)) * -1 : 0,
+    // });
   }
+
   return (
     <>
       <div className="App">
         <div className="wrapper">
-          <Input result={result} text={text} />
+          <Input result={result} text={!calc ? text : `-${text}`} />
           <div className="rows">
-            <Buttons symbol="7" handleClick={addToText} />
+            <Buttons symbol={7} handleClick={addToText} />
             <Buttons symbol="8" handleClick={addToText} />
             <Buttons symbol="9" handleClick={addToText} />
             <Buttons symbol="/" color="#fc5908" handleClick={addToText} />
